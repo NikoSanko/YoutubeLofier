ec7472e782d45cd0cf40848cc90fe2692ab2c222
NikoSanko <ninikoko2525p@gmail.com>
2025-04-12 08:45:02 +0900

    add: compressor

--- js/content.js
+++ js/content.js
@@ -6,6 +6,7 @@ const AUDIO_MODES = {
 
 const EFFECT_TYPE = {
   EQ: "eq",
+  COMPRESSOR: "compressor",
   SATURATOR: "saturator"
 }
 
@@ -18,17 +19,19 @@ const EQ_TYPE = {
 
 const EFFECT_PRESETS = {
   [AUDIO_MODES.VINYL]: [
-    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 40, sampleRate: null, mixRatio: 0.5 } },
+    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 40, sampleRate: null, mixRatio: 0.1 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.HIGHPASS, frequency: 100, q: 12, gain: null, mixRatio: 1 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.LOWPASS, frequency: 18000, q: 12, gain: null, mixRatio: 1 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.LOWSHELF, frequency: 500, q: null, gain: -5, mixRatio: 1 } },
-    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 10, sampleRate: 0.5, mixRatio: 0.1 } }
+    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 10, sampleRate: 0.5, mixRatio: 0.1 } },
+    { type: EFFECT_TYPE.COMPRESSOR, params: { threshold: -3, knee: 10, ratio: 5, attack: 0, release: 0, mixRatio: 1 }}
   ],
   [AUDIO_MODES.RADIO]: [
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.HIGHPASS, frequency: 150, q: 12, gain: null, mixRatio: 0.5 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.LOWPASS, frequency: 6700, q: 8, gain: null, mixRatio: 0.8 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.LOWSHELF, frequency: 500, q: null, gain: -3, mixRatio: 1 } },
-    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 13, sampleRate: 0.25, mixRatio: 0.4 } }
+    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 13, sampleRate: 0.25, mixRatio: 0.4 } },
+    { type: EFFECT_TYPE.COMPRESSOR, params: { threshold: -3, knee: 10, ratio: 5, attack: 0, release: 0, mixRatio: 1 }}
   ],
   [AUDIO_MODES.GUNK]: [
     { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 20, sampleRate: null, mixRatio: 0.2 } },
@@ -36,7 +39,7 @@ const EFFECT_PRESETS = {
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.LOWPASS, frequency: 18000, q: 36, gain: null, mixRatio: 1 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.HIGHSHELF, frequency: 14000, q: null, gain: -12, mixRatio: 1 } },
     { type: EFFECT_TYPE.EQ, params: { type: EQ_TYPE.LOWSHELF, frequency: 1000, q: null, gain: -8, mixRatio: 1 } },
-    { type: EFFECT_TYPE.SATURATOR, params: { distortionAmount: 10, sampleRate: 0.1, mixRatio: 0.1 } }
+    { type: EFFECT_TYPE.COMPRESSOR, params: { threshold: -3, knee: 10, ratio: 10, attack: 0, release: 0, mixRatio: 1 }}
   ]
 };
 
@@ -78,6 +81,8 @@ class AudioProcessor {
     preset.forEach(effect => {
       if (effect.type === EFFECT_TYPE.EQ) {
         this.insertEQ(effect.params);
+      } else if (effect.type == EFFECT_TYPE.COMPRESSOR) {
+        this.insertCompressor(effect.params);
       } else if (effect.type === EFFECT_TYPE.SATURATOR) {
         this.insertSaturator(effect.params);
       }
@@ -92,6 +97,18 @@ class AudioProcessor {
     eq.gain.setValueAtTime(gain, this.context.currentTime);
 
     this.effectors.push(eq);
+    this.mixRatios.push(mixRatio);
+  }
+
+  insertCompressor({ threshold, knee, ratio, attack, release, mixRatio }) {
+    const compressor = this.context.createDynamicsCompressor();
+    compressor.threshold.setValueAtTime(threshold, this.context.currentTime);
+    compressor.knee.setValueAtTime(knee, this.context.currentTime);
+    compressor.ratio.setValueAtTime(ratio, this.context.currentTime);
+    compressor.attack.setValueAtTime(attack, this.context.currentTime);
+    compressor.release.setValueAtTime(release, this.context.currentTime);
+  
+    this.effectors.push(compressor);
     this.mixRatios.push(mixRatio);
   }
 
