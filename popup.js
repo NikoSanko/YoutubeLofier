b8a396df8053a3277f37d3ef3733f2a98806d880
NikoSanko <ninikoko2525p@gmail.com>
2025-04-12 10:31:34 +0900

    refactor: フォールバック画像やアイコン画像を差し替え

--- popup.js
+++ popup.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
       var thumbnailUrl = "https://i.ytimg.com/vi/" + videoId + "/mqdefault.jpg";
       $('#cdImage').attr('src', thumbnailUrl);
     } else {
-      $('#cdImage').attr('src', "images/lasa.jpg");
+      $('#cdImage').attr('src', "images/cdFallbackImage.png");
     }
   });
 });
